4714de5ee75cd10bdecd2da09754b3669c6aa674
Jayavel <jayavel1520@gmail.com>
2024-07-04 23:22:12 +0530

    fix : error fixed

--- cutoff.js
+++ cutoff.js
@@ -1,13 +1,13 @@
-var maths_mark=document.getElementById("maths")
-var physics_mark=document.getElementById("physics")
-var chemistry_mark=document.getElementById("chemistry")
-var cutoff=document.getElementById("cutoff")
+let maths_mark=document.getElementById("maths")
+let physics_mark=document.getElementById("physics")
+let chemistry_mark=document.getElementById("chemistry")
+let cutoff=document.getElementById("cutoff")
 
 function calculate()
 {
-   var Maths = maths_mark.value
-   var Physics = physics_mark.value
-   var Chemistry = chemistry_mark.value
-   var Cutoff=Number(Maths)+(Number(Physics)+Number(Chemistry))/2
+   let Maths = maths_mark.value
+   let Physics = physics_mark.value
+   let Chemistry = chemistry_mark.value
+   let Cutoff=Number(Maths)+(Number(Physics)+Number(Chemistry))/2
    cutoff.textContent=" YOUR CUT OFF : "+Cutoff+"/200"
 }
